54c7cad157c232feb01f0b039b338595043f7805
Tiago Xavier da Cunha Almeida <tiagoxdca@mbp-de-tiago.lan>
2018-09-24 10:49:20 +0100

    Initial Commit

---
+++ UnitTestsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestsDemo
+//
+//  Created by Tiago Xavier da Cunha Almeida on 24/09/18.
+//  Copyright © 2018 Tiago Xavier da Cunha Almeida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ UnitTestsDemoTests/UnitTestsDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  UnitTestsDemoTests.swift
+//  UnitTestsDemoTests
+//
+//  Created by Tiago Xavier da Cunha Almeida on 24/09/18.
+//  Copyright © 2018 Tiago Xavier da Cunha Almeida. All rights reserved.
+//
+
+import XCTest
+@testable import UnitTestsDemo
+
+class UnitTestsDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
